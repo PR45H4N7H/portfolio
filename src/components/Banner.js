ac0447f983939624808ac69d97b8f7789e9628d6
Prashanth Reddy <112146124+PR45H4N7H@users.noreply.github.com>
2025-07-26 23:50:13 -0400

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,8 +56,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Prashanth Reddy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "a Full Stack Web Developer", "looking out for opportunities" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>A Northwest Missouri State University alumni, I'm currently seeking roles in full-stack web development, front-end, or backend development. Eager to apply my skills and passion for technology, I bring a strong educational background and a commitment to innovation to any dynamic team.</p>
+                <h1>{`Hi! I'm Prashanth Reddy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "a Data Analyst", "looking out for opportunities" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>A Northwest Missouri State University alumni, I'm currently seeking roles in Data Analyst, Provider Analyst, or Data Engineer. Eager to apply my skills and passion for technology, I bring a strong educational background and a commitment to innovation to any dynamic team.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
