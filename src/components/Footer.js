aad77523b349abdb875bb0e7d87c3085943a26ef
Prashanth Reddy <112146124+PR45H4N7H@users.noreply.github.com>
2025-08-04 14:48:34 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-//import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
@@ -10,20 +9,33 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          {/* { <MailchimpForm /> } */}
+          {/* <MailchimpForm /> */}
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/prashanthreddy-/"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/pr45h4n7h_r3ddy/?igsh=MTN2OGE1bTE5Z3JyMw%3D%3D#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/prashanth-da/">
+                <img src={navIcon1} alt="LinkedIn" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="Placeholder" />
+              </a>
+              <a href="https://www.instagram.com/pr45h4n7h_r3ddy/?igsh=MTN2OGE1bTE5Z3JyMw%3D%3D#">
+                <img src={navIcon3} alt="Instagram" />
+              </a>
             </div>
-            
+
+            {/* 🔥 Visitor Counter Badge */}
+            <div style={{ marginTop: "10px" }}>
+              <img
+                src="https://api.visitorbadge.io/api/visitors?path=portfolio-theta-swart-10.vercel.app&label=Visitors&countColor=%23ffffff&labelColor=%2300ffaa&style=flat-square"
+                alt="Visitor Count"
+              />
+            </div>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
